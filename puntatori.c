64eb08e8f2045ab565be7c48d7792566c9e6d1c2
ceoEnsdrongo <150335445+ceoEsdrongo@users.noreply.github.com>
2024-04-23 09:12:08 +0200

    puntatori.c

---
+++ puntatori.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+int main(){
+        /*int numero = 10;
+        int* puntatore = &numero;
+        (*puntatore)++;
+        int*puntatore = *puntatore*2;
+        printf("inserisci un nuovo valore");
+        scanf("%d", &numero);
+        printf("valore della variabile: %d\n",numero);
+        */
+
+
+       /*float array []={2.5,3.7,1.2,5.9,4.3};
+       float* puntatore = array;
+       float somma =0;
+       for(int i=0;i<5;i++){
+        somma+=*(puntatore+i);
+        printf("somma degli elementi dell'array: %f\n", somma);*/
+
+
+        /*int lunghezza, vettore[100];
+        printf("inserisci la lunghezza dell'array: ");
+        scanf("%d", &lunghezza);
+        int *p= vettore;
+        printf("inserisci elementi del vettore:\n");
+        for(int i=0;i<lunghezza;i++){
+            scanf("%d",p);
+            p++;
+        }
+        printf("elementi dell'array\n");
+        for(int i=0;i<lunghezza;i++){
+            printf("%d", vettore[i]);
+        }*/
+       
+       /*char* puntatore;
+       char stringa[] ="Mi chiamo Tony e gioco a hokey*";
+       puntatore=stringa;
+       printf("stringa: %s",puntatore);*/
+    return 0;
+}
